e72b590981d76b3bd473ae3ea6335b3fb06a6803
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 21:26:54 -0300

    style(dto): fix pint formatting in signature appearance xobject dto
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Application/DTO/SignatureAppearanceXObjectDto.php
+++ src/Application/DTO/SignatureAppearanceXObjectDto.php
@@ -7,7 +7,7 @@ namespace SignerPHP\Application\DTO;
 final readonly class SignatureAppearanceXObjectDto
 {
     /**
-     * @param  string  $stream     PDF content stream operators for the n2 layer
+     * @param  string  $stream  PDF content stream operators for the n2 layer
      * @param  array<string, mixed>|null  $resources  Resource dictionary (fonts, XObjects, etc.)
      */
     public function __construct(
